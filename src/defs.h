a04c17f6863352a00128ba25506e685fbc83f765
Anton Dorodnitsyn <avdorodn@gs66-atorus.gsfc.nasa.gov>
2018-03-14 11:00:35 -0400

    ready to calc

--- src/defs.h
+++ src/defs.h
@@ -20,8 +20,7 @@
  *  (user modified via configure) */
 
 /* Version identifier when configure was last run */
-
-#define CONFIGURE_DATE "Tue Jan  2 12:20:14 EST 2018"
+#define CONFIGURE_DATE "Wed Mar 14 10:58:39 EDT 2018"
 
 /* Problem generator with which Athena is compiled */
 #define A_PROBLEM "torus10"
