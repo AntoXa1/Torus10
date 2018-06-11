36cc9d1e7ef5cb17b436d3ea136ebb50443237b1
Anton Dorodnitsyn <avdorodn@gs66-atorus.gsfc.nasa.gov>
2018-06-11 14:21:37 -0400

    1

--- conmake_linux.sh
+++ conmake_linux.sh
@@ -2,17 +2,20 @@
 
 echo Torus10 config amd compile script 
 
-make all clean
+#make all clean
 
 ./configure --with-coord=cylindrical \
  --with-problem=torus10 --with-gas=mhd --with-flux=hlld \
---with-order=2p --enable-mpi --with-integrator=vl --enable-fft \
+--with-order=2p --enable-mpi --with-integrator=vl \
 --enable-fofc 
 
+# --with-order=2p --enable-mpi --with-integrator=vl --enable-fft \
 
 # make all MACHINE=macosx
 
-make all MACHINE=macosxmpi
+# make all MACHINE=macosxmpi
+
+#make all MACHINE=atorusmpi
 
 
 # ./configure --with-coord=cylindrical \
